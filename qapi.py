ec5d9dfa26a7223d320c3df11dada042a4d763a2
HCLivess <admin@bismuth.cz>
2019-11-23 15:08:32 +0100

    small edits

--- qapi.py
+++ qapi.py
@@ -80,16 +80,17 @@ class PairMarket:
         self.spread_percentage = 100 - part_percentage(self.bid, self.ask)
 
 
-
 def pick_currency(balances_dict, currency_name):
     for entry in balances_dict["data"]["balances"]:
         if entry["currency"] == currency_name:
             return Balance(entry)
 
+
 class Balance:
     def __init__(self, balance_dict):
         self.name = balance_dict["currency"]
         self.balance = float(balance_dict["balance"])
+
 
 class Config:
     def __init__(
@@ -128,16 +129,15 @@ def age(timestamp):
     return int(time.time() - epoch_ts)
 
 
-
-
-
 if __name__ == "__main__":
 
     # Create a session object to make repeated API calls easy!
     api = requests.Session()
     # Create an authenticator with your API key
 
-    api.auth_native = QtradeAPI("https://api.qtrade.io", key=load_credentials())  # use in the future
+    api.auth_native = QtradeAPI(
+        "https://api.qtrade.io", key=load_credentials()
+    )  # use in the future
     api.auth = QtradeAuth(load_credentials())
 
     # load currencies
@@ -197,7 +197,7 @@ if __name__ == "__main__":
                 pair_orders.base_balance = order_api["data"]["base_balance"]
                 pair_orders.closed_orders = order_api["data"]["closed_orders"]
                 pair_orders.market_balance = order_api["data"]["market_balance"]
-                #pair_orders.open_orders = order_api["data"]["open_orders"]  # old way
+                # pair_orders.open_orders = order_api["data"]["open_orders"]  # old way
                 pair_orders.open_orders = api.auth_native.orders(open=True)
 
                 print(pair_orders.base_balance)
@@ -215,7 +215,6 @@ if __name__ == "__main__":
                     balances = api.get("https://api.qtrade.io/v1/user/balances").json()
                     print(balances)
 
-
                     # place a sell order
                     if pair_market.ask <= conf.min_sell_price:
                         print("Market price too low to sell now")
@@ -225,9 +224,9 @@ if __name__ == "__main__":
                         )
 
                     else:
-                        c = pick_currency(balances, conf.name)
+                        currency = pick_currency(balances, conf.name)
                         # print(balance["balance"])
-                        if c.balance > conf.sell_amount:
+                        if currency.balance > conf.sell_amount:
 
                             # sell order
                             req = {
@@ -245,7 +244,7 @@ if __name__ == "__main__":
                             conf.orders_placed.append(order_id)
                         else:
                             print(
-                                f"Insufficient balance ({c.balance} for {c.name} ({conf.buy_amount} units)"
+                                f"Insufficient balance ({currency.balance} for {currency.name} ({conf.buy_amount} units)"
                             )
 
                     # place a sell order
@@ -256,11 +255,11 @@ if __name__ == "__main__":
                     elif conf.buy_amount <= 0:
                         print(f"Not configured to buy (buy set to {conf.buy_amount})")
                     else:
-                        c = pick_currency(balances, "BTC")
+                        currency = pick_currency(balances, "BTC")
 
                         # print(balance["balance"])
                         if (
-                            c.balance > conf.buy_amount * pair_market.bid
+                            currency.balance > conf.buy_amount * pair_market.bid
                         ):  # if one can afford to buy trade_buy_amount
 
                             # sell order
@@ -281,7 +280,7 @@ if __name__ == "__main__":
                             conf.orders_placed.append(order_id)
                         else:
                             print(
-                                f"Insufficient balance ({c.balance}) for {c.name} ({conf.buy_amount} orders)"
+                                f"Insufficient balance ({currency.balance}) for {currency.name} ({conf.buy_amount} orders)"
                             )
 
                     # place a buy order
